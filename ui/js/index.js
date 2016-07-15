fc6e02ac68e11e5bc29d31f78c80f8264899a14b
Eric Betts <bettse@fastmail.fm>
2016-07-15 15:23:21 -0700

    Custom icon for pokestops from https://owl.yt/pokego/map.html

--- ui/js/index.js
+++ ui/js/index.js
@@ -72,12 +72,25 @@ function simplestyle(f, latlon) {
           size.charAt(0) + symbol + '+' + color +
           ((window.devicePixelRatio === 2) ? '@2x' : '') +
           '.png';
-    return new L.Marker(latlon, {
-        icon: new L.icon({
-            iconUrl: url,
-            iconSize: sizes[size],
-            iconAnchor: [sizes[size][0] / 2, sizes[size][1] / 2],
-            popupAnchor: [sizes[size][0] / 2, 0]
-        })
-    });
+    if (fp.type == 'pokestop') {
+      return new L.Marker(latlon, {
+          icon: pokestopIcon
+      })
+    } else {
+      return new L.Marker(latlon, {
+          icon: new L.icon({
+              iconUrl: url,
+              iconSize: sizes[size],
+              iconAnchor: [sizes[size][0] / 2, sizes[size][1] / 2],
+              popupAnchor: [sizes[size][0] / 2, 0]
+          })
+      });
+    }
 }
+
+var pokestopIcon = L.icon({
+    iconUrl: 'img/pokestop.png',
+    iconSize:     [32, 48], // size of the icon
+    iconAnchor:   [16, 48], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -58] // point from which the popup should open relative to the iconAnchor
+});
