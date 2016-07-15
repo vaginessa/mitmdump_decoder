fe2a9bf2458162f2d63fa4218b1813af1211d912
Eric Betts <bettse@fastmail.fm>
2016-07-15 09:03:51 -0700

    New tileset, Stamen.Watercolor

--- ui/index.js
+++ ui/index.js
@@ -1,8 +1,12 @@
 var map = L.map('map');
 
+L.tileLayer.provider('Stamen.Watercolor').addTo(map);
+
+/*
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+*/
 
 var filters = document.getElementById('filters');
 filters.onclick = function() {
